2a74c1285091c1f27044e7ae63d524abfb0de8d5
sajid.ali <sajid.ali@gllfl.com>
2024-05-10 19:04:08 +0530

    added Booking service

---
+++ SkTravelingBookingService/src/main/java/com/SkTravelingReservationService/SkTravelingReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.SkTravelingReservationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkTravelingReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkTravelingReservationServiceApplication.class, args);
+	}
+
+}
---
+++ SkTravelingBookingService/src/test/java/com/SkTravelingReservationService/SkTravelingReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SkTravelingReservationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkTravelingReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
